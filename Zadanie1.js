79ebee38f392baaff5f4871a9af133a5cc13ce40
kaleo320 <kamilmajcherczyk@op.pl>
2023-01-09 15:32:34 +0100

    tak

---
+++ Zadanie1.js
@@ -0,0 +1,8 @@
+const tak = document.querySelector('header');
+console.log(tak);
+tak.style.backgroundColor="Yellow";
+//2
+const tak1=document.querySelector('header h1');
+console.log(tak1)
+tak1.style.fontStyle="Arial Black";
+tak1.innerHTML="Preambula";
